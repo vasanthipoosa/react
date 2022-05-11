d520894bc8c5ba3a13a48c7c5ce46fc1114280bd
vasanthi.poosa <puusavasu@gmail.com>
2022-05-12 01:06:58 +0530

    first commit

---
+++ src/Function.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './index1.css';
+
+const Function=()=> {
+  return (
+    <div id='container'>
+  <h1>This is created using functional component</h1>
+  <p>This is done using external css</p>
+  <p style={{color:'blue'}}>This is done using inline css</p>
+
+  
+      </div>
+  )
+}
+export default Function
